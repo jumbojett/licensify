dad0a1b6ef78ab580fa6b61e7d2a9687539c78e5
Michael Jett <mjett@mitre.org>
2015-12-04 19:45:56 +0000

    Fixing logic

--- src/Eo/Licensify/Command/LicensifyCommand.php
+++ src/Eo/Licensify/Command/LicensifyCommand.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the ACE (Analytics Collaboration Environment) package.
+ * This file is part of the forked package.
  *
- * (c) 2015 The MITRE Corporation
+ * (c) 2015 Michael Jett
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
